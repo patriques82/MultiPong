84778d695c20b62b5ce0fbc1d92437274d2212bd
Patrik Nygren <patriknygren82@gmail.com>
2016-12-22 19:56:11 +0100

    factory method worldproperties with helper functions for messageFacory done

--- src/main/java/com/multipong/server/MessageFactory.java
+++ src/main/java/com/multipong/server/MessageFactory.java
@@ -15,47 +15,22 @@ public class MessageFactory {
 	
 	public static WorldProperties worldProperties(String pos) {
 		WorldProperties prop = new WorldProperties();
-		// World
 		prop.width = Options.getWorldWidth();
 		prop.height = Options.getWorldHeight();
-		// Ball
 		prop.ball = ballMessage(prop.width, prop.height);
-		// other
 		prop.other = paddleMessage(opposite(pos));
-		// your
-		prop.your = new PaddleMessage();
-		prop.your.position = pos;
-		prop.your.height = Conf.PADDLE_THICKNESS;
-		prop.your.width = Conf.PADDLE_LENGTH;
-		prop.your.x = Conf.PADDLE_BOTTOM_UPPER_LEFT_X;
-		prop.your.y = Conf.PADDLE_BOTTOM_UPPER_LEFT_Y;
+		prop.your = paddleMessage(pos);
 		return prop;
 	}
 
-	private static PaddleMessage paddleMessage(String opposite) {
-		PaddleMessage other = new PaddleMessage();
-		other.position = opposite;
-		other.width = paddleWidth(opposite);
-		other.height = paddleHeight(opposite);
-		other.x = paddleXPos(opposite); 
-		other.y = paddleYPos(opposite);
-		return other;
-	}
-
-	private static int paddleYPos(String opposite) {
-		return 0;
-	}
-
-	private static int paddleXPos(String opposite) {
-		return 0;
-	}
-
-	private static int paddleHeight(String opposite) {
-		return 0;
-	}
-
-	private static int paddleWidth(String opposite) {
-		return 0;
+	private static BallMessage ballMessage(int width, int height) {
+		BallMessage ballMessage = new BallMessage();
+		ballMessage.x = width/2;
+		ballMessage.y = height/2;
+		ballMessage.d = Options.getBallDiameter();
+		ballMessage.vx = Options.getBallVx();
+		ballMessage.vy = Options.getBallVy();
+		return ballMessage;
 	}
 
 	private static String opposite(String pos) {
@@ -71,14 +46,54 @@ public class MessageFactory {
 			throw new IllegalArgumentException("Unknown position");
 	}
 
-	private static BallMessage ballMessage(int width, int height) {
-		BallMessage ballMessage = new BallMessage();
-		ballMessage.x = width/2;
-		ballMessage.y = height/2;
-		ballMessage.d = Options.getBallDiameter();
-		ballMessage.vx = Options.getBallVx();
-		ballMessage.vy = Options.getBallVy();
-		return ballMessage;
+	private static PaddleMessage paddleMessage(String pos) {
+		PaddleMessage other = new PaddleMessage();
+		other.position = pos;
+		other.width = paddleWidth(pos);
+		other.height = paddleHeight(pos);
+		other.x = paddleXPos(pos); 
+		other.y = paddleYPos(pos);
+		return other;
+	}
+
+	private static int paddleWidth(String pos) {
+		if(pos.equals("up") || pos.equals("bottom"))
+			return Options.getPaddleLength();
+		else if(pos.equals("left") || pos.equals("right"))
+			return Options.getPaddleThickness();
+		else
+			throw new IllegalArgumentException("Unknown position");
+	}
+
+	private static int paddleHeight(String pos) {
+		if(pos.equals("up") || pos.equals("bottom"))
+			return Options.getPaddleThickness();
+		else if(pos.equals("left") || pos.equals("right"))
+			return Options.getPaddleLength();
+		else
+			throw new IllegalArgumentException("Unknown position");
+	}
+
+	private static int paddleXPos(String pos) {
+		if(pos.equals("up") || pos.equals("bottom"))
+			return Options.getWorldWidth()/2 - Options.getPaddleLength()/2;
+		else if(pos.equals("left"))
+			return 0;
+		else if(pos.equals("right"))
+			return Options.getWorldWidth() - Options.getPaddleThickness();
+		else
+			throw new IllegalArgumentException("Unknown position");
+	}
+
+	private static int paddleYPos(String pos) {
+		if(pos.equals("up"))
+			return 0;
+		else if(pos.equals("bottom"))
+			return Options.getWorldHeight() - Options.getPaddleThickness();
+		else if(pos.equals("left") || pos.equals("right"))
+			return Options.getWorldHeight()/2 - Options.getPaddleLength()/2;
+		else
+			throw new IllegalArgumentException("Unknown position");
 	}
 	
 }
